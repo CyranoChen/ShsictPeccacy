2e6ad0a30c8086bb3689357a5308aabc41aa0fb8
Cyrano <cyrano@arsenalcn.com>
2018-02-22 18:38:58 +0800

    Remove the reference of mvc

--- Shsict.Peccancy.Console/Program.cs
+++ Shsict.Peccancy.Console/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
-using Shsict.Peccancy.Mvc.Models;
+using Shsict.Peccancy.Service.DbHelper;
 using Shsict.Peccancy.Service.Extension;
 using Shsict.Peccancy.Service.Logger;
 using Shsict.Peccancy.Service.Scheduler;
@@ -16,28 +16,31 @@ namespace Shsict.Peccancy.Console
         {
             IAppLog log = new AppLog();
 
-            var schedulers = Schedule.Cache.ScheduleList.FindAll(x => x.IsActive && x.Seconds > 0);
-
-            if (schedulers.Count > 0)
+            using (IRepository repo = new Repository())
             {
-                var warningScheduler =
-                    schedulers.FindAll(x => DateTime.Now > x.LastCompletedTime.AddSeconds(x.Seconds + 300));
+                var schedulers = repo.All<Schedule>().FindAll(x => x.IsActive && x.Seconds > 0);
 
-                if (warningScheduler.Count > 0)
+                if (schedulers.Count > 0)
                 {
-                    var content = new StringBuilder();
-                    foreach (var s in warningScheduler)
-                    {
-                        content.Append(new
-                        {
-                            ScheduleName = s.ScheduleKey,
-                            LastCompletedTime = s.LastCompletedTime.ToString("yyyyMMdd HH:mm:ss"),
-                            Interval = s.Seconds
-                        }.ToJson());
-                        content.Append("/r/n");
-                    }
+                    var warningScheduler =
+                        schedulers.FindAll(x => DateTime.Now > x.LastCompletedTime.AddSeconds(x.Seconds + 300));
 
-                    SendEmail(ConfigGlobal.AdminEmail, "外集卡违章数据同步停止提醒", content.ToString());
+                    if (warningScheduler.Count > 0)
+                    {
+                        var content = new StringBuilder();
+                        foreach (var s in warningScheduler)
+                        {
+                            content.Append(new
+                            {
+                                ScheduleName = s.ScheduleKey,
+                                LastCompletedTime = s.LastCompletedTime.ToString("yyyyMMdd HH:mm:ss"),
+                                Interval = s.Seconds
+                            }.ToJson());
+                            content.Append("/r/n");
+                        }
+
+                        SendEmail(ConfigGlobal.AdminEmail, "外集卡违章数据同步停止提醒", content.ToString());
+                    }
                 }
             }
 
@@ -60,21 +63,21 @@ namespace Shsict.Peccancy.Console
             }
 
             /* 上面3个参数分别是发件人地址（可以随便写），发件人姓名，编码*/
-            mail.From = new MailAddress("SMS@shsict.com", "外集卡违章数据同步监控", System.Text.Encoding.UTF8);
+            mail.From = new MailAddress("SMS@shsict.com", "外集卡违章数据同步监控", Encoding.UTF8);
 
             mail.Subject = subject;//邮件标题 
-            mail.SubjectEncoding = System.Text.Encoding.UTF8;//邮件标题编码 
+            mail.SubjectEncoding = Encoding.UTF8;//邮件标题编码 
 
             mail.Body = "以下内容为系统自动发送，请勿直接回复，谢谢。/r/n/r/n";//邮件内容 
             mail.Body += content;
-            mail.BodyEncoding = System.Text.Encoding.UTF8;//邮件内容编码 
+            mail.BodyEncoding = Encoding.UTF8;//邮件内容编码 
 
             mail.IsBodyHtml = false;//是否是HTML邮件 
             mail.Priority = MailPriority.High;//邮件优先级 
 
             var client = new SmtpClient
             {
-                Credentials = new System.Net.NetworkCredential("SMS@shsict.com", "Sms@2018"),
+                Credentials = new NetworkCredential("SMS@shsict.com", "Sms@2018"),
                 Host = "lotus.shsict.com"
             };
 
